f6de4f05e48743e588a07f33c709897f4a0e7ce6
wurtylex <anthony.c.chang10@gmail.com>
2026-05-31 12:29:15 -0700

    upper case A

--- ECC/Basic.lean
+++ ECC/Basic.lean
@@ -1,6 +1,6 @@
 import Mathlib.Data.Set.Basic
 
-variable (α : Type _) (n : ℕ)
+variable (Α : Type _) (n : ℕ)
 
 -- Code of blocklength n over α is a subset α^n
-def Code : Type _ := Set (Fin n → α)
+def Code : Type _ := Set (Fin n → Α)
